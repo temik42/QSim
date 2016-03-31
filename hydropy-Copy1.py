a81702d5d7a2a1822357e1605bb28f256cde1935
temik42 <ikiru@inbox.ru>
2016-03-31 15:31:18 +0400

    normalized variables

--- hydropy-Copy1.py
+++ hydropy-Copy1.py
@@ -37,19 +37,7 @@ class hydro(threading.Thread):
     def __init__(self,dt,X,A,rho,rhou,rhoe,hrate,gamma=5./3,maxiter=1e3,each=0):
         threading.Thread.__init__(self)
         
-        
-        self.sct = 1e6
-        self.scn = 1e9
-        self.mu = Sun.mu_c*Sun.m_p
-        self.scx = np.sqrt(Sun.k_b/(Sun.mu_c*Sun.m_p)*self.sct)   
-        self.scrho = self.scn*self.mu
-        self.scm = self.scrho*self.scx**3 
-        self.scrhou = self.scrho*self.scx
-        self.scrhoe = self.scrhou*self.scx
-        self.scrl = self.scn**2/self.scrhoe
-        
-        
-        self.X = X/self.scx
+        self.X = X
         self.A = A
         Xi = np.zeros_like(self.X)
         
@@ -67,19 +55,21 @@ class hydro(threading.Thread):
         self.L = self.s[-1]
         
         self.dt = dt
-        self.rho = rho*self.A/self.scrho
-        self.rhou = rhou*self.A/self.scrhou
-        self.rhoe = rhoe*self.A/self.scrhoe
+        self.rho = rho*self.A
+        self.rhou = rhou*self.A
+        self.rhoe = rhoe*self.A
         self._hrate = hrate
-        self.g = -d(self.X[:,2])/self.ds*Sun.g_sun/self.scx
+        self.g = -d(self.X[:,2])/self.ds*Sun.g_sun
         self.gamma = gamma
-        self.kappa = Sun.kappa*self.A*self.sct**3.5/self.scx**2/self.scrhoe
+        self.kappa = Sun.kappa*self.A
+        self.k_b = Sun.k_b
         self.maxiter = np.int(maxiter)
         
         self.nx = self.dx.shape[0]
-        self.T0 = 2e4/self.sct
-        self.rl0 = 1.31*self.scrhoe
-        
+        self.m = Sun.mu_c*Sun.m_p
+        self.T0 = 2e4
+        #self.rho0 = 1e10*self.m
+        #self.p0 = 2*self.rho0/self.m*Sun.k_b*self.T0
         
         self.time = 0     
         self.each = each
@@ -92,7 +82,7 @@ class hydro(threading.Thread):
         self.status = 'initialized'
     
     def hrate(self,time):
-        return self._hrate(time)*self.A/self.scrhoe
+        return self._hrate(time)*self.A
         
     
     def load_rl(self):
@@ -101,7 +91,7 @@ class hydro(threading.Thread):
         self.rlTemperature = 10**rl['temperature']   
     
     def radloss(self, T):
-        return np.interp(T*self.sct, self.rlTemperature, self.rlRate, left = 0, right = 0)*self.A*self.scrl
+        return np.interp(T, self.rlTemperature, self.rlRate, left = 0, right = 0)/self.A
     
     def d_dx(self,q,order = 1):
         if (order == 1):
@@ -118,13 +108,21 @@ class hydro(threading.Thread):
             
 
     def advect(self,q):
+        #dq = q - np.roll(q,1)
+        #r = np.where(self.ui >= 0., (np.roll(q,1)-np.roll(q,2))/dq, (np.roll(q,-1)-q)/dq)
+        #a = np.min([[np.ones(self.nx)],[2*r]], axis = 0)
+        #b = np.min([[2*np.ones(self.nx)],[r]], axis = 0)
+        #phi = np.max([[np.zeros(self.nx)],[a],[b]], axis = 0)
+        
         flux = np.where(self.ui >= 0., self.ui*np.roll(q,1),self.ui*q)
+        #flux = flux + 0.5*np.abs(self.ui)*(1-np.abs(self.ui*self.dt/self.dx))*phi*dq
+        
         q -= (self.dt*(np.roll(flux,-1)-flux)/self.dxi)
         
 
     def diffuse(self):
         
-        dd = self.kappa*self.T**2.5/3/self.rho
+        dd = self.kappa*self.T**2.5*self.m/(3*self.k_b)/self.rho
         z = 2./7*self.kappa*self.T**3.5
 
         D = -1/self.dt-2*dd/(self.dx*self._dx)
@@ -165,8 +163,8 @@ class hydro(threading.Thread):
         self.rhoe[:2] = self.p[:2]/(self.gamma-1.)
         self.rhoe[-2:] = self.p[-2:]/(self.gamma-1.)
         
-        self.rho[:2] = self.p[:2]/(2*self.T[:2])
-        self.rho[-2:] = self.p[-2:]/(2*self.T[-2:])
+        self.rho[:2] = self.p[:2]/(2*self.k_b/self.m*self.T[:2])
+        self.rho[-2:] = self.p[-2:]/(2*self.k_b/self.m*self.T[-2:])
         
         
     def boundary(self):
@@ -179,27 +177,31 @@ class hydro(threading.Thread):
         #self.rho[0] = self.rho[1]
         #self.rho[-1] = self.rho[-2]
         
-        self.rho[0] = self.p[0]/(2*self.T[0])
-        self.rho[-1] = self.p[-1]/(2*self.T[-1])
+        self.rho[0] = self.p[0]/(2*self.k_b/self.m*self.T[0])
+        self.rho[-1] = self.p[-1]/(2*self.k_b/self.m*self.T[-1])
         
         self.rhoe[0] = self.p[0]/(self.gamma-1.)
         self.rhoe[-1] = self.p[-1]/(self.gamma-1.)
         
         self.rhou[0] = self.rho[0]*(4./7*(0.5*self.kappa[0]*(self.T[1]**2.5+self.T[0]**2.5)*(self.T[1]-self.T[0])/self.dx[1]-
-                                    self.rl0*self.p[0]**2*self.dx[1])/self.p[0]-
+                                     1e-31*(self.p[0]/2/self.k_b)**2*self.dx[1])/self.p[0]-
                                     3./7*self.rhou[1]/self.rho[1])
         self.rhou[-1] = self.rho[-1]*(4./7*(0.5*self.kappa[-1]*(self.T[-2]**2.5+self.T[-1]**2.5)*(self.T[-1]-self.T[-2])/self.dx[-1]+
-                                    self.rl0*self.p[-1]**2*self.dx[-1])/self.p[-1]-
+                                     1e-31*(self.p[-1]/2/self.k_b)**2*self.dx[-1])/self.p[-1]-
                                     3./7*self.rhou[-2]/self.rho[-2])
 
     def getptu(self):
         self.u = self.rhou/self.rho
+        #self.ui = 0.5*(self.u + np.roll(self.u,1))
+        #self.ui[0] = 0.
         
         self.etot = self.rhoe/self.rho
         self.ekin = self.u**2/2
         self.eth = self.etot - self.ekin
-        self.p = (self.gamma-1.)*self.rho*3*T
-        self.T = self.eth/3
+        self.p = (self.gamma-1.)*self.rho*self.eth
+        #self.p = (self.gamma-1.)*self.rhoe
+        self.T = (self.eth*self.m/(3*self.k_b))
+        #self.T = (self.p/self.rho*self.m/(2*Sun.k_b))
         
     def getui(self):
         self.ui = 0.5*(self.u + np.roll(self.u,1))
@@ -223,12 +225,12 @@ class hydro(threading.Thread):
         self.getptu()
         self.boundary()
         self.getui()
-        
+
         q = np.where(self.ui >= 0., self.ui*np.roll(self.p,1),self.ui*self.p)
         self.rhoe -= self.dt*(np.roll(q,-1)-q)/self.dxi
         self.rhou -= self.dt*(np.roll(self.p,-1) - np.roll(self.p,1))/2/self.dxi
          
-        self.rhoe -= self.dt*self.rho**2*self.radloss(self.T)
+        self.rhoe -= self.dt*(self.rho/self.m)**2*self.radloss(self.T)
         self.rhoe += self.dt*self.rhou*self.g
         self.rhoe += self.dt*self.hrate(self.time)
         self.rhou += self.dt*(np.roll(self.A,-1) - np.roll(self.A,1))/2/self.dxi/self.A*self.p
@@ -251,27 +253,27 @@ class hydro(threading.Thread):
             self.time += self.dt
             if (self.each != 0):
                 if (i % self.each == 0):
-                    I1 = (self.rho/self.A/self.mu*self.scrho)**2*AIA.t171(self.T*self.sct)*1e8
-                    I2 = (self.rho/self.A/self.mu*self.scrho)**2*AIA.t193(self.T*self.sct)*1e8
-                    I3 = (self.rho/self.A/self.mu*self.scrho)**2*AIA.t211(self.T*self.sct)*1e8
-                    self.I171 += [np.sum((I1*self.dx*self.scx)[np.where(np.abs(self.s-self.L*0.5) < self.L*0.5-self.L*0.15)])/self.L]
-                    self.I193 += [np.sum((I2*self.dx*self.scx)[np.where(np.abs(self.s-self.L*0.5) < self.L*0.5-self.L*0.15)])/self.L]
-                    self.I211 += [np.sum((I3*self.dx*self.scx)[np.where(np.abs(self.s-self.L*0.5) < self.L*0.5-self.L*0.15)])/self.L]
+                    I1 = (self.rho/self.A/self.m)**2*AIA.t171(self.T)*1e8
+                    I2 = (self.rho/self.A/self.m)**2*AIA.t193(self.T)*1e8
+                    I3 = (self.rho/self.A/self.m)**2*AIA.t211(self.T)*1e8
+                    self.I171 += [np.sum((I1*self.dx)[np.where(np.abs(self.s-self.L*0.5) < self.L*0.5-self.L*0.15)])/self.L]
+                    self.I193 += [np.sum((I2*self.dx)[np.where(np.abs(self.s-self.L*0.5) < self.L*0.5-self.L*0.15)])/self.L]
+                    self.I211 += [np.sum((I3*self.dx)[np.where(np.abs(self.s-self.L*0.5) < self.L*0.5-self.L*0.15)])/self.L]
                     
                     plt.subplot(211)
                     plt.title('Density 10^4 / Temperature', size = 18)
-                    plt.plot(self.s*self.scx, np.log10(self.rho/self.A/self.mu*self.scrho)-4)
-                    plt.plot(self.s*self.scx, np.log10(self.T*self.sct))
-                    plt.axis([0,np.max(self.s)*self.scx,4,7])
+                    plt.plot(self.s, np.log10(self.rho/self.A/self.m)-4)
+                    plt.plot(self.s, np.log10(self.T))
+                    plt.axis([0,np.max(self.s),4,7])
                     #plt.axis([0,1e9,4,7])
                     plt.subplot(212)
                     #plt.plot(self.s, self.rhou)
                     #plt.axis([0,1e9,-1e-8,1e-8])
                     plt.title('AIA 171/193/211 intensity', size = 18)
-                    plt.plot(self.s*self.scx,np.log10(I1))
-                    plt.plot(self.s*self.scx,np.log10(I2))
-                    plt.plot(self.s*self.scx,np.log10(I3))
-                    plt.axis([0,np.max(self.s)*self.scx,-2,4])
+                    plt.plot(self.s,np.log10(I1))
+                    plt.plot(self.s,np.log10(I2))
+                    plt.plot(self.s,np.log10(I3))
+                    plt.axis([0,np.max(self.s),-2,4])
                     
                     fname = (img_dir + str(np.int(i/self.each)).zfill(np.ceil(np.log10(self.maxiter/self.each)).astype(np.int)) + '.png')
                     fig.savefig(fname)
--- hydropy1.py
+++ hydropy1.py
@@ -34,30 +34,16 @@ def trisol(D,U,V,B):
 
 
 class hydro(threading.Thread):
-    def __init__(self,dt,X,A,rho,rhou,rhoe,hrate,gamma=5./3,maxiter=1e3,each=0):
+    def __init__(self,dt,X,rho,rhou,rhoe,hrate,gamma=5./3,maxiter=1e3,each=0):
         threading.Thread.__init__(self)
-        
-        
-        self.sct = 1e6
-        self.scn = 1e9
-        self.mu = Sun.mu_c*Sun.m_p
-        self.scx = np.sqrt(Sun.k_b/(Sun.mu_c*Sun.m_p)*self.sct)   
-        self.scrho = self.scn*self.mu
-        self.scm = self.scrho*self.scx**3 
-        self.scrhou = self.scrho*self.scx
-        self.scrhoe = self.scrhou*self.scx
-        self.scrl = self.scn**2/self.scrhoe
-        
-        
-        self.X = X/self.scx
-        self.A = A
+        self.X = X
         Xi = np.zeros_like(self.X)
         
         for i in range(0,3):
-            Xi[:,i] = 0.5*(self.X[:,i] + np.roll(self.X[:,i],1))
+            Xi[:,i] = 0.5*(X[:,i] + np.roll(X[:,i],1))
          
         self.ds = np.sqrt(np.sum([d(self.X[:,i])**2 for i in range(0,3)],0))
-        self.dx = np.sqrt(np.sum([(np.roll(self.X[:,i],1)-self.X[:,i])**2 for i in range(0,3)],0))
+        self.dx = np.sqrt(np.sum([(np.roll(X[:,i],1)-X[:,i])**2 for i in range(0,3)],0))
         self.dx[0] = self.dx[1]
         self._dx = np.roll(self.dx,-1)
         self.ddx = self.dx*self._dx*(self.dx+self._dx)
@@ -67,19 +53,19 @@ class hydro(threading.Thread):
         self.L = self.s[-1]
         
         self.dt = dt
-        self.rho = rho*self.A/self.scrho
-        self.rhou = rhou*self.A/self.scrhou
-        self.rhoe = rhoe*self.A/self.scrhoe
-        self._hrate = hrate
-        self.g = -d(self.X[:,2])/self.ds*Sun.g_sun/self.scx
+        self.rho = rho
+        self.rhou = rhou
+        self.rhoe = rhoe
+        self.hrate = hrate
+        self.g = -d(X[:,2])/self.ds*Sun.g_sun
         self.gamma = gamma
-        self.kappa = Sun.kappa*self.A*self.sct**3.5/self.scx**2/self.scrhoe
         self.maxiter = np.int(maxiter)
         
         self.nx = self.dx.shape[0]
-        self.T0 = 2e4/self.sct
-        self.rl0 = 1.31*self.scrhoe
-        
+        self.m = Sun.mu_c*Sun.m_p
+        self.T0 = 2e4
+        self.rho0 = 1e10*self.m
+        self.p0 = 2*self.rho0/self.m*Sun.k_b*self.T0
         
         self.time = 0     
         self.each = each
@@ -90,18 +76,14 @@ class hydro(threading.Thread):
         
         self.load_rl()
         self.status = 'initialized'
-    
-    def hrate(self,time):
-        return self._hrate(time)*self.A/self.scrhoe
         
-    
     def load_rl(self):
         rl = np.load(path + '\\radloss.npz')
         self.rlRate = 10**rl['rlRate']
         self.rlTemperature = 10**rl['temperature']   
     
     def radloss(self, T):
-        return np.interp(T*self.sct, self.rlTemperature, self.rlRate, left = 0, right = 0)*self.A*self.scrl
+        return np.interp(T, self.rlTemperature, self.rlRate, left = 0, right = 0)
     
     def d_dx(self,q,order = 1):
         if (order == 1):
@@ -118,14 +100,22 @@ class hydro(threading.Thread):
             
 
     def advect(self,q):
+        #dq = q - np.roll(q,1)
+        #r = np.where(self.ui >= 0., (np.roll(q,1)-np.roll(q,2))/dq, (np.roll(q,-1)-q)/dq)
+        #a = np.min([[np.ones(self.nx)],[2*r]], axis = 0)
+        #b = np.min([[2*np.ones(self.nx)],[r]], axis = 0)
+        #phi = np.max([[np.zeros(self.nx)],[a],[b]], axis = 0)
+        
         flux = np.where(self.ui >= 0., self.ui*np.roll(q,1),self.ui*q)
+        #flux = flux + 0.5*np.abs(self.ui)*(1-np.abs(self.ui*self.dt/self.dx))*phi*dq
+        
         q -= (self.dt*(np.roll(flux,-1)-flux)/self.dxi)
         
 
     def diffuse(self):
         
-        dd = self.kappa*self.T**2.5/3/self.rho
-        z = 2./7*self.kappa*self.T**3.5
+        dd = Sun.kappa*self.T**2.5*self.m/(3*Sun.k_b)/self.rho
+        z = 2./7*Sun.kappa*self.T**3.5
 
         D = -1/self.dt-2*dd/(self.dx*self._dx)
         D[0] = 1.
@@ -165,8 +155,8 @@ class hydro(threading.Thread):
         self.rhoe[:2] = self.p[:2]/(self.gamma-1.)
         self.rhoe[-2:] = self.p[-2:]/(self.gamma-1.)
         
-        self.rho[:2] = self.p[:2]/(2*self.T[:2])
-        self.rho[-2:] = self.p[-2:]/(2*self.T[-2:])
+        self.rho[:2] = self.p[:2]/(2*Sun.k_b/self.m*self.T[:2])
+        self.rho[-2:] = self.p[-2:]/(2*Sun.k_b/self.m*self.T[-2:])
         
         
     def boundary(self):
@@ -179,27 +169,31 @@ class hydro(threading.Thread):
         #self.rho[0] = self.rho[1]
         #self.rho[-1] = self.rho[-2]
         
-        self.rho[0] = self.p[0]/(2*self.T[0])
-        self.rho[-1] = self.p[-1]/(2*self.T[-1])
+        self.rho[0] = self.p[0]/(2*Sun.k_b/self.m*self.T[0])
+        self.rho[-1] = self.p[-1]/(2*Sun.k_b/self.m*self.T[-1])
         
         self.rhoe[0] = self.p[0]/(self.gamma-1.)
         self.rhoe[-1] = self.p[-1]/(self.gamma-1.)
         
-        self.rhou[0] = self.rho[0]*(4./7*(0.5*self.kappa[0]*(self.T[1]**2.5+self.T[0]**2.5)*(self.T[1]-self.T[0])/self.dx[1]-
-                                    self.rl0*self.p[0]**2*self.dx[1])/self.p[0]-
+        self.rhou[0] = self.rho[0]*(4./7*(0.5*Sun.kappa*(self.T[1]**2.5+self.T[0]**2.5)*(self.T[1]-self.T[0])/self.dx[1]-
+                                     1e-31*(self.p[0]/2/Sun.k_b)**2*self.dx[1])/self.p[0]-
                                     3./7*self.rhou[1]/self.rho[1])
-        self.rhou[-1] = self.rho[-1]*(4./7*(0.5*self.kappa[-1]*(self.T[-2]**2.5+self.T[-1]**2.5)*(self.T[-1]-self.T[-2])/self.dx[-1]+
-                                    self.rl0*self.p[-1]**2*self.dx[-1])/self.p[-1]-
+        self.rhou[-1] = self.rho[-1]*(4./7*(0.5*Sun.kappa*(self.T[-2]**2.5+self.T[-1]**2.5)*(self.T[-1]-self.T[-2])/self.dx[-1]+
+                                     1e-31*(self.p[-1]/2/Sun.k_b)**2*self.dx[-1])/self.p[-1]-
                                     3./7*self.rhou[-2]/self.rho[-2])
 
     def getptu(self):
         self.u = self.rhou/self.rho
+        #self.ui = 0.5*(self.u + np.roll(self.u,1))
+        #self.ui[0] = 0.
         
         self.etot = self.rhoe/self.rho
         self.ekin = self.u**2/2
         self.eth = self.etot - self.ekin
-        self.p = (self.gamma-1.)*self.rho*3*T
-        self.T = self.eth/3
+        self.p = (self.gamma-1.)*self.rho*self.eth
+        #self.p = (self.gamma-1.)*self.rhoe
+        self.T = (self.eth*self.m/(3*Sun.k_b))
+        #self.T = (self.p/self.rho*self.m/(2*Sun.k_b))
         
     def getui(self):
         self.ui = 0.5*(self.u + np.roll(self.u,1))
@@ -223,15 +217,18 @@ class hydro(threading.Thread):
         self.getptu()
         self.boundary()
         self.getui()
-        
+
         q = np.where(self.ui >= 0., self.ui*np.roll(self.p,1),self.ui*self.p)
         self.rhoe -= self.dt*(np.roll(q,-1)-q)/self.dxi
+        #self.rhoe -= self.dt*self.d_dx(self.p*self.u)
+        #self.rhou -= self.dt*self.d_dx(self.p)
+        
+        #self.rhoe -= self.dt*(np.roll(self.p*self.u,-1) - np.roll(self.p*self.u,1))/2/self.dxi
         self.rhou -= self.dt*(np.roll(self.p,-1) - np.roll(self.p,1))/2/self.dxi
-         
-        self.rhoe -= self.dt*self.rho**2*self.radloss(self.T)
+        
+        self.rhoe -= self.dt*(self.rho/self.m)**2*self.radloss(self.T)
         self.rhoe += self.dt*self.rhou*self.g
         self.rhoe += self.dt*self.hrate(self.time)
-        self.rhou += self.dt*(np.roll(self.A,-1) - np.roll(self.A,1))/2/self.dxi/self.A*self.p
              
         self.rhou += self.dt*self.rho*self.g    
 
@@ -251,27 +248,27 @@ class hydro(threading.Thread):
             self.time += self.dt
             if (self.each != 0):
                 if (i % self.each == 0):
-                    I1 = (self.rho/self.A/self.mu*self.scrho)**2*AIA.t171(self.T*self.sct)*1e8
-                    I2 = (self.rho/self.A/self.mu*self.scrho)**2*AIA.t193(self.T*self.sct)*1e8
-                    I3 = (self.rho/self.A/self.mu*self.scrho)**2*AIA.t211(self.T*self.sct)*1e8
-                    self.I171 += [np.sum((I1*self.dx*self.scx)[np.where(np.abs(self.s-self.L*0.5) < self.L*0.5-self.L*0.15)])/self.L]
-                    self.I193 += [np.sum((I2*self.dx*self.scx)[np.where(np.abs(self.s-self.L*0.5) < self.L*0.5-self.L*0.15)])/self.L]
-                    self.I211 += [np.sum((I3*self.dx*self.scx)[np.where(np.abs(self.s-self.L*0.5) < self.L*0.5-self.L*0.15)])/self.L]
+                    I1 = (self.rho/self.m)**2*AIA.t171(self.T)*1e8
+                    I2 = (self.rho/self.m)**2*AIA.t193(self.T)*1e8
+                    I3 = (self.rho/self.m)**2*AIA.t211(self.T)*1e8
+                    self.I171 += [np.sum((I1*self.dx)[np.where(np.abs(self.s-self.L*0.5) < self.L*0.5-self.L*0.15)])/self.L]
+                    self.I193 += [np.sum((I2*self.dx)[np.where(np.abs(self.s-self.L*0.5) < self.L*0.5-self.L*0.15)])/self.L]
+                    self.I211 += [np.sum((I3*self.dx)[np.where(np.abs(self.s-self.L*0.5) < self.L*0.5-self.L*0.15)])/self.L]
                     
                     plt.subplot(211)
                     plt.title('Density 10^4 / Temperature', size = 18)
-                    plt.plot(self.s*self.scx, np.log10(self.rho/self.A/self.mu*self.scrho)-4)
-                    plt.plot(self.s*self.scx, np.log10(self.T*self.sct))
-                    plt.axis([0,np.max(self.s)*self.scx,4,7])
+                    plt.plot(self.s, np.log10(self.rho/self.m)-4)
+                    plt.plot(self.s, np.log10(self.T))
+                    plt.axis([0,np.max(self.s),4,7])
                     #plt.axis([0,1e9,4,7])
                     plt.subplot(212)
                     #plt.plot(self.s, self.rhou)
                     #plt.axis([0,1e9,-1e-8,1e-8])
                     plt.title('AIA 171/193/211 intensity', size = 18)
-                    plt.plot(self.s*self.scx,np.log10(I1))
-                    plt.plot(self.s*self.scx,np.log10(I2))
-                    plt.plot(self.s*self.scx,np.log10(I3))
-                    plt.axis([0,np.max(self.s)*self.scx,-2,4])
+                    plt.plot(self.s,np.log10(I1))
+                    plt.plot(self.s,np.log10(I2))
+                    plt.plot(self.s,np.log10(I3))
+                    plt.axis([0,np.max(self.s),-2,4])
                     
                     fname = (img_dir + str(np.int(i/self.each)).zfill(np.ceil(np.log10(self.maxiter/self.each)).astype(np.int)) + '.png')
                     fig.savefig(fname)
